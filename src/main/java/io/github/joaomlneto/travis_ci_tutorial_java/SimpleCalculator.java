4593a9ae18078fbad5c5de1cec5ca85f37f2bf2d
Fodor-Pap Balázs <fodorpapbalazs01@gmail.com>
2019-03-23 19:16:55 +0100

    add bugfix

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -2,10 +2,10 @@ package io.github.joaomlneto.travis_ci_tutorial_java;
 
 public class SimpleCalculator {
 	public int add(int a, int b) {
-		return a - b;
+		return a + b;
 	}
 	public int sub(int a, int b) {
-		return a + b;
+		return a - b;
 	}
 	public int mul(int a, int b) {
 		return a * b;
